6b71d580c1c0906f88453c935bd6859632f35c64
Manish <[manishbulchandani4@gmail.com]>
2025-01-24 15:19:16 +0530

    token expiring fix

--- server/utils/signJwt.js
+++ server/utils/signJwt.js
@@ -1,9 +1,13 @@
 import jwt from 'jsonwebtoken';
 
-const signToken = (id,role) => {
-  return jwt.sign({ id ,role}, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN,
-  });
+// const signToken = (id,role) => {
+//   return jwt.sign({ id ,role}, process.env.JWT_SECRET, {
+//     expiresIn: process.env.JWT_EXPIRES_IN,
+//   });
+// };
+
+const signToken = (id, role) => {
+  return jwt.sign({ id, role }, process.env.JWT_SECRET);
 };
 
 export default signToken;
